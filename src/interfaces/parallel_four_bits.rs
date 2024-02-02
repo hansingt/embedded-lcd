1e159fa8747760bc8329ae777b0d06ef020c3907
Torben Hansing <torbenhansing@gmail.com>
2024-02-02 18:03:19 +0100

    Working library

--- src/interfaces/parallel_four_bits.rs
+++ src/interfaces/parallel_four_bits.rs
@@ -2,177 +2,228 @@ use core::fmt::Debug;
 
 use embedded_hal::{blocking::delay::DelayUs, digital::v2::OutputPin};
 
-use super::{Interface, Font, Lines};
-
-use esp_println::println;
+use super::{Font, Interface, Lines};
 
 #[derive(Debug)]
-pub enum Parallel4BitsError<D0Error, D1Error, D2Error, D3Error, ENError, RSError>
+pub enum Parallel4BitsError<D7Error, D6Error, D5Error, D4Error, ENError, RSError>
 where
-    D0Error: Debug,
-    D1Error: Debug,
-    D2Error: Debug,
-    D3Error: Debug,
+    D7Error: Debug,
+    D6Error: Debug,
+    D5Error: Debug,
+    D4Error: Debug,
     ENError: Debug,
     RSError: Debug,
 {
     ENError(ENError),
     RSError(RSError),
-    D0Error(D0Error),
-    D1Error(D1Error),
-    D2Error(D2Error),
-    D3Error(D3Error),
+    D7Error(D7Error),
+    D6Error(D6Error),
+    D5Error(D5Error),
+    D4Error(D4Error),
 }
 
 pub struct Parallel4Bits<
-    D0,
-    D1,
-    D2,
-    D3,
+    D7,
+    D6,
+    D5,
+    D4,
     EN,
     RS,
-    D0Error,
-    D1Error,
-    D2Error,
-    D3Error,
+    D7Error,
+    D6Error,
+    D5Error,
+    D4Error,
     ENError,
     RSError,
     Delay,
 > where
-    D0: OutputPin<Error = D0Error>,
-    D1: OutputPin<Error = D1Error>,
-    D2: OutputPin<Error = D2Error>,
-    D3: OutputPin<Error = D3Error>,
+    D7: OutputPin<Error = D7Error>,
+    D6: OutputPin<Error = D6Error>,
+    D5: OutputPin<Error = D5Error>,
+    D4: OutputPin<Error = D4Error>,
     EN: OutputPin<Error = ENError>,
     RS: OutputPin<Error = RSError>,
     Delay: DelayUs<u16>,
 {
-    d0: D0,
-    d1: D1,
-    d2: D2,
-    d3: D3,
+    d7: D7,
+    d6: D6,
+    d5: D5,
+    d4: D4,
     en: EN,
     rs: RS,
     delay: Delay,
 }
 
-impl<D0, D1, D2, D3, EN, RS, D0Error, D1Error, D2Error, D3Error, ENError, RSError, Delay>
-    Parallel4Bits<D0, D1, D2, D3, EN, RS, D0Error, D1Error, D2Error, D3Error, ENError, RSError, Delay>
+impl<D7, D6, D5, D4, EN, RS, D7Error, D6Error, D5Error, D4Error, ENError, RSError, Delay>
+    Parallel4Bits<
+        D7,
+        D6,
+        D5,
+        D4,
+        EN,
+        RS,
+        D7Error,
+        D6Error,
+        D5Error,
+        D4Error,
+        ENError,
+        RSError,
+        Delay,
+    >
 where
-    D0: OutputPin<Error = D0Error>,
-    D1: OutputPin<Error = D1Error>,
-    D2: OutputPin<Error = D2Error>,
-    D3: OutputPin<Error = D3Error>,
+    D7: OutputPin<Error = D7Error>,
+    D6: OutputPin<Error = D6Error>,
+    D5: OutputPin<Error = D5Error>,
+    D4: OutputPin<Error = D4Error>,
     EN: OutputPin<Error = ENError>,
     RS: OutputPin<Error = RSError>,
-    D0Error: Debug,
-    D1Error: Debug,
-    D2Error: Debug,
-    D3Error: Debug,
+    D7Error: Debug,
+    D6Error: Debug,
+    D5Error: Debug,
+    D4Error: Debug,
     ENError: Debug,
     RSError: Debug,
     Delay: DelayUs<u16>,
 {
-    pub fn new(d0: D0, d1: D1, d2: D2, d3: D3, en: EN, rs: RS, delay: Delay) -> Self {
+    pub fn new(d7: D7, d6: D6, d5: D5, d4: D4, en: EN, rs: RS, delay: Delay) -> Self {
         Parallel4Bits {
-            d0,
-            d1,
-            d2,
-            d3,
+            d7,
+            d6,
+            d5,
+            d4,
             en,
             rs,
             delay,
         }
     }
 
-    fn write_word(
+    fn write_nibble(
         &mut self,
         data: u8,
-    ) -> Result<(), Parallel4BitsError<D0Error, D1Error, D2Error, D3Error, ENError, RSError>> { 
-        println!("WRITE WORD: {data:04b}");
+    ) -> Result<(), Parallel4BitsError<D7Error, D6Error, D5Error, D4Error, ENError, RSError>> {
         // Set the data bits
-        match data & 0b0001 {
-            1 => self.d0.set_high().or_else(|err| Err(Parallel4BitsError::D0Error(err))),
-            _ => self.d0.set_low().or_else(|err| Err(Parallel4BitsError::D0Error(err))),
-        }?;
-        match data & 0b0010 {
-            1 => self.d1.set_high().or_else(|err| Err(Parallel4BitsError::D1Error(err))),
-            _ => self.d1.set_low().or_else(|err| Err(Parallel4BitsError::D1Error(err))),
+        match data & 0b1000 {
+            0 => self
+                .d7
+                .set_low()
+                .or_else(|err| Err(Parallel4BitsError::D7Error(err))),
+            _ => self
+                .d7
+                .set_high()
+                .or_else(|err| Err(Parallel4BitsError::D7Error(err))),
         }?;
         match data & 0b0100 {
-            1 => self.d2.set_high().or_else(|err| Err(Parallel4BitsError::D2Error(err))),
-            _ => self.d2.set_low().or_else(|err| Err(Parallel4BitsError::D2Error(err))),
+            0 => self
+                .d6
+                .set_low()
+                .or_else(|err| Err(Parallel4BitsError::D6Error(err))),
+            _ => self
+                .d6
+                .set_high()
+                .or_else(|err| Err(Parallel4BitsError::D6Error(err))),
         }?;
-        match data & 0b1000 {
-            1 => self.d3.set_high().or_else(|err| Err(Parallel4BitsError::D3Error(err))),
-            _ => self.d3.set_low().or_else(|err| Err(Parallel4BitsError::D3Error(err))),
+        match data & 0b0010 {
+            0 => self
+                .d5
+                .set_low()
+                .or_else(|err| Err(Parallel4BitsError::D5Error(err))),
+            _ => self
+                .d5
+                .set_high()
+                .or_else(|err| Err(Parallel4BitsError::D5Error(err))),
+        }?;
+        match data & 0b0001 {
+            0 => self
+                .d4
+                .set_low()
+                .or_else(|err| Err(Parallel4BitsError::D4Error(err))),
+            _ => self
+                .d4
+                .set_high()
+                .or_else(|err| Err(Parallel4BitsError::D4Error(err))),
         }?;
         // Open the latch
-        self.en.set_high().or_else(|err| Err(Parallel4BitsError::ENError(err)))?;
+        self.en
+            .set_high()
+            .or_else(|err| Err(Parallel4BitsError::ENError(err)))?;
         self.delay.delay_us(1);
-       
         // Close the latch
-        self.en.set_low().or_else(|err| Err(Parallel4BitsError::ENError(err)))?;
-        self.delay.delay_us(40);
+        self.en
+            .set_low()
+            .or_else(|err| Err(Parallel4BitsError::ENError(err)))?;
         Ok(())
-
     }
 }
 
-impl<D0, D1, D2, D3, EN, RS, D0Error, D1Error, D2Error, D3Error, ENError, RSError, Delay> Interface
-    for Parallel4Bits<D0, D1, D2, D3, EN, RS, D0Error, D1Error, D2Error, D3Error, ENError, RSError, Delay>
+impl<D7, D6, D5, D4, EN, RS, D7Error, D6Error, D5Error, D4Error, ENError, RSError, Delay> Interface
+    for Parallel4Bits<
+        D7,
+        D6,
+        D5,
+        D4,
+        EN,
+        RS,
+        D7Error,
+        D6Error,
+        D5Error,
+        D4Error,
+        ENError,
+        RSError,
+        Delay,
+    >
 where
-    D0: OutputPin<Error = D0Error>,
-    D1: OutputPin<Error = D1Error>,
-    D2: OutputPin<Error = D2Error>,
-    D3: OutputPin<Error = D3Error>,
+    D7: OutputPin<Error = D7Error>,
+    D6: OutputPin<Error = D6Error>,
+    D5: OutputPin<Error = D5Error>,
+    D4: OutputPin<Error = D4Error>,
     EN: OutputPin<Error = ENError>,
     RS: OutputPin<Error = RSError>,
-    D0Error: Debug,
-    D1Error: Debug,
-    D2Error: Debug,
-    D3Error: Debug,
+    D7Error: Debug,
+    D6Error: Debug,
+    D5Error: Debug,
+    D4Error: Debug,
     ENError: Debug,
     RSError: Debug,
     Delay: DelayUs<u16>,
 {
-    type Error = Parallel4BitsError<D0Error, D1Error, D2Error, D3Error, ENError, RSError>;
-
+    type Error = Parallel4BitsError<D7Error, D6Error, D5Error, D4Error, ENError, RSError>;
 
     fn initialize(&mut self, lines: Lines, font: Font) -> Result<(), Self::Error> {
-        self.write_word(0b0011)?;
-        println!("WAIT 45ms");
+        self.write_nibble(0b0011)?;
         self.delay.delay_us(4500);
-        self.write_word(0b0011)?;
-        println!("WAIT 150us");
+        self.write_nibble(0b0011)?;
         self.delay.delay_us(150);
-        self.write_word(0b0011)?;
+        self.write_nibble(0b0011)?;
 
-        self.write_word(0b0010)?;
-        
+        self.write_nibble(0b0010)?;
+
         let function_set = match font {
             Font::FiveTimesTenDots => 0b0010_0100,
             Font::FiveTimesEightDots => match lines {
                 Lines::One => 0b0010_0000,
                 Lines::Two => 0b0010_1000,
-            }
+            },
         };
-        println!("");
         self.write(function_set, true)
     }
 
     fn write(&mut self, data: u8, command: bool) -> Result<(), Self::Error> {
         match command {
-            true => self.rs.set_low().or_else(|err| Err(Parallel4BitsError::RSError(err))),
-            false => self.rs.set_high().or_else(|err| Err(Parallel4BitsError::RSError(err))),
+            true => self
+                .rs
+                .set_low()
+                .or_else(|err| Err(Parallel4BitsError::RSError(err))),
+            false => self
+                .rs
+                .set_high()
+                .or_else(|err| Err(Parallel4BitsError::RSError(err))),
         }?;
 
         // Write the upper word
-        self.write_word(data >> 4)?;
+        self.write_nibble(data >> 4)?;
 
         // Then write the lower word
-        self.write_word(data & 0b0000_1111)?;
-        println!("");
+        self.write_nibble(data & 0b0000_1111)?;
         Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use core::fmt::{Error, Write};
 
 mod interfaces;
 
+use embedded_hal::blocking::delay::DelayUs;
 pub use interfaces::*;
 
 pub enum ShiftDirection {
@@ -11,20 +12,28 @@ pub enum ShiftDirection {
     Left = 0b0000_0100,
 }
 
-
 #[derive(Debug)]
-pub struct LCD1602<I: Interface> {
+pub struct LCD1602<I: Interface, Delay: DelayUs<u16>> {
     interface: I,
+    delay: Delay,
 }
 
-impl<I: Interface> LCD1602<I> {
-    pub fn new(interface: I, lines: Lines, font: Font) -> Result<Self, I::Error> {
-        let mut lcd = Self { interface };
+impl<I: Interface, Delay: DelayUs<u16>> LCD1602<I, Delay> {
+    pub fn new(interface: I, lines: Lines, font: Font, delay: Delay) -> Result<Self, I::Error> {
+        let mut lcd = Self { interface, delay };
         lcd.interface.initialize(lines, font)?;
+        lcd.clear()?;
         Ok(lcd)
     }
 
-    pub fn configure(&mut self, enable_display: bool, enable_cursor: bool, cursor_blink: bool, shift_display: bool, shift_direction: ShiftDirection) -> Result<(), I::Error> {
+    pub fn configure(
+        &mut self,
+        enable_display: bool,
+        enable_cursor: bool,
+        cursor_blink: bool,
+        shift_display: bool,
+        shift_direction: ShiftDirection,
+    ) -> Result<(), I::Error> {
         let display_control = match enable_display {
             true => 0b0000_1100,
             false => 0b0000_1000,
@@ -36,7 +45,7 @@ impl<I: Interface> LCD1602<I> {
             false => 0b0000_1000,
         };
         self.interface.write(display_control, true)?;
-        
+
         let entry_mode = match shift_display {
             true => shift_direction as u8 | 0x01,
             false => shift_direction as u8,
@@ -46,15 +55,22 @@ impl<I: Interface> LCD1602<I> {
 
     #[inline]
     pub fn clear(&mut self) -> Result<(), I::Error> {
-        self.interface.write(0b0000_0001, true)
+        self.interface.write(0b0000_0001, true)?;
+        self.delay.delay_us(100);
+        Ok(())
     }
 
     #[inline]
     pub fn home(&mut self) -> Result<(), I::Error> {
-        self.interface.write(0b0000_0010, true)
+        self.interface.write(0b0000_0010, true)?;
+        Ok(())
     }
 
-    pub fn shift(&mut self, shift_display: bool, shift_direction: ShiftDirection) -> Result<(), I::Error> {
+    pub fn shift(
+        &mut self,
+        shift_display: bool,
+        shift_direction: ShiftDirection,
+    ) -> Result<(), I::Error> {
         let mut cmd = match shift_display {
             true => 0b0001_1000,
             false => 0b0001_0000,
@@ -82,7 +98,7 @@ impl<I: Interface> LCD1602<I> {
     }
 }
 
-impl<I: Interface> Write for LCD1602<I> {
+impl<I: Interface, D: DelayUs<u16>> Write for LCD1602<I, D> {
     fn write_char(&mut self, c: char) -> core::fmt::Result {
         match c.is_ascii() {
             true => match self.write_byte(c as u8) {
